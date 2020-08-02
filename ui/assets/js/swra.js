895ca495a70fab1dc63e07c8daf883d7704d7851
Scott Wichser <blast007@users.sourceforge.net>
2020-08-01 20:19:10 -0500

    UI code cleanups.
    Make use of the Echo Reverse function for generating URLs. Create
    reusable JavaScript for the update and delete actions. Use Cleave.js for
    MAC address input formatting.

---
+++ ui/assets/js/swra.js
@@ -0,0 +1,28 @@
+$(function () {
+    // Select2.js multiple select boxes
+    if (jQuery.fn.select2)
+        $('select[multiple]').select2();
+
+    // Cleave input formatting
+    if (Cleave) {
+        new Cleave('.mac-input', {
+            delimiter: ':',
+            blocks: new Array(6).fill(2),
+            uppercase: true,
+        });
+    }
+
+    // Submit a form via XHR when a value of an input changes
+    $('.submit-on-change').change(function(ev) {
+        var form = $(ev.target.form);
+        $.post(form.attr('action'), form.serialize())
+    });
+
+    // Confirm deletion before submitting form
+    $('.confirm-delete').submit(function(ev) {
+        var type = $(ev.target).data('type');
+        var name = $(ev.target).data('name');
+        if (!confirm("Do you really want to delete the '" + name + "' " + type + "?"))
+            ev.preventDefault();
+    });
+});
